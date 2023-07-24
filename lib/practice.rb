032622162d96c4884ec9abc6221a0f5d7b05b845
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-07-24 14:58:03 -0600

    Reformat

--- lib/practice.rb
+++ lib/practice.rb
@@ -7,17 +7,9 @@ def calculate(input)
 end
 
 def apply_discount(json, percentage)
-  # iterate through each object
-  # grab cashback
-  # apply discount
-  # round to integer
-  # return offer
-  # require 'pry'; binding.pry
   json[:offers].each do |offer|
-    # require 'pry'; binding.pry
     discount = offer[:cashback] * (BigDecimal(percentage)/100)
     offer[:cashback] = (offer[:cashback] - discount).floor
   end
-  # require 'pry'; binding.pry
   json
 end
